25a209f930deb5596ce9cc800a89416615c7d704
AAGuedes <airam.a.g.85@gmail.com>
2023-04-26 13:14:59 +0100

    Paths de las rutas faltantes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
-import { HeroesPageComponent } from './pages/heroes-page/heroes-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
@@ -11,7 +11,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 @NgModule({
   declarations: [
-    HeroesPageComponent,
+    HeroPageComponent,
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
