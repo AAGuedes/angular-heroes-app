3ef111e55a655c23cb303673acbece1dd4392d6c
AAGuedes <airam.a.g.85@gmail.com>
2023-05-01 23:59:30 +0100

    Autenticar y mostrar usuario activo

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -29,9 +29,13 @@ export class AuthService {
     return this.http.get<User>(`${this.baseUrl}/users/1`)
       .pipe(
         tap(user => this.user = user),
-        tap(user => localStorage.setItem('token', user.id.toString()))
+        tap(user => localStorage.setItem('token', 'asdvafbhnghmj.asdvafvbafb.asdasdbasb'))
       );
   }
 
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
+  }
 
 }
